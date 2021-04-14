21981203f6550394d0d6d981faef96559edb17a9
maikofelix47 <maikofelix89@gmail.com>
2021-04-14 09:26:09 +0300

    Added ETL node sync script

--- service/sync-service.js
+++ service/sync-service.js
@@ -11,144 +11,39 @@ const def = {
 }
 
 function updateHivSummary(){
-
-  return new Promise((resolve,reject) => {
   const sql = `call build_flat_hiv_summary("build",600,100,1);`;
-  console.log("sql", sql);
-  connection
-    .getConnectionPool()
-    .then((pool) => {
-      pool.query(sql, function (error, results, fields) {
-        if (error) {
-          console.log("Error", error);
-          reject(error);
-        } else {
-          console.log("Update Hiv Summary successfull: ", results);
-          resolve(results);
-        }
-      });
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-      reject(error);
-    });
-
-  });
-  
+  return runSqlScript(sql);  
 }
 
 function updateFlatAppointment(){
-
-  return new Promise((resolve,reject) => {
-  const sql = `call build_flat_appointment("build",800,100,1);`;
-  console.log("sql", sql);
-  connection
-    .getConnectionPool()
-    .then((pool) => {
-      pool.query(sql, function (error, results, fields) {
-        if (error) {
-          console.log("Error", error);
-          reject(error);
-        } else {
-          console.log("Update Flat Appointment successfull: ", results);
-          resolve(results);
-        }
-      });
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-      reject(error);
-    });
-
-  });
-  
+  const sql = `call etl.build_flat_appointment("build",800,100,1);`;
+  return runSqlScript(sql);    
 }
 
 function updateFlatObs(){
-
-  return new Promise((resolve,reject) => {
-  const sql = `call generate_flat_obs_v_1_3();`;
-  console.log("sql", sql);
-  connection
-    .getConnectionPool()
-    .then((pool) => {
-      pool.query(sql, function (error, results, fields) {
-        if (error) {
-          console.log("Error", error);
-          reject(error);
-        } else {
-          console.log("Update Flat Obs successfull: ", results);
-          resolve(results);
-        }
-      });
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-      reject(error);
-    });
-
-  });
-  
+  const sql = `call etl.generate_flat_obs_v_1_3();`;
+  return runSqlScript(sql);    
 }
 
 
 function updateFlatLabObs(){
-
-  return new Promise((resolve,reject) => {
   const sql = `call generate_flat_lab_obs();`;
-  console.log("sql", sql);
-  connection
-    .getConnectionPool()
-    .then((pool) => {
-      pool.query(sql, function (error, results, fields) {
-        if (error) {
-          console.log("Error", error);
-          reject(error);
-        } else {
-          console.log("Update Flat Lab Obs successfull: ", results);
-          resolve(results);
-        }
-      });
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-      reject(error);
-    });
-
-  });
-  
+  return runSqlScript(sql);  
 }
 
 function updateFlatOrders(){
-
-  return new Promise((resolve,reject) => {
-  const sql = `call generate_flat_orders();`;
-  console.log("sql", sql);
-  connection
-    .getConnectionPool()
-    .then((pool) => {
-      pool.query(sql, function (error, results, fields) {
-        if (error) {
-          console.log("Error", error);
-          reject(error);
-        } else {
-          console.log("Update Flat Orders successfull: ", results);
-          resolve(results);
-        }
-      });
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-      reject(error);
-    });
-
-  });
-  
+  const sql = `call etl.generate_flat_orders();`;
+  return runSqlScript(sql);
 }
 
 function updateLabsAndImaging(){
+  const sql = `call etl.generate_flat_labs_and_imaging("sync",1,15000,100);`
+  return runSqlScript(sql);
+}
+
+function runSqlScript(sqlQuery){
   return new Promise((resolve,reject) => {
-    const sql = `call generate_flat_labs_and_imaging("sync",1,15000,100);`;
+    const sql = sqlQuery;
     console.log("sql", sql);
     connection
       .getConnectionPool()
@@ -169,6 +64,7 @@ function updateLabsAndImaging(){
       });
   
     });
+
 }
 
 module.exports = def;
