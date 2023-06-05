4515ab91101fb2dfebb35d990a850aa81b3014c0
Kipchumba C. Bett <kbett68@gmail.com>
2023-06-05 15:59:43 +0300

    Feat: Enhance console logs

--- app/base-sync.js
+++ app/base-sync.js
@@ -4,17 +4,17 @@ const { runStoredProc } = require('./runner');
 
 const updateFlatObs = async () => {
   const sql = `call etl.generate_flat_obs_v_1_3();`;
-  return await runStoredProc(sql);
+  return await runStoredProc({ procedure: sql, name: 'Flat Obs'});
 };
 
 const updateFlatLabObs = async () => {
   const sql = `call generate_flat_lab_obs();`;
-  return await runStoredProc(sql);
+  return await runStoredProc({ procedure: sql, name: 'Flat Lab Obs'});
 };
 
 const updateFlatOrders = async () => {
   const sql = `call etl.generate_flat_orders();`;
-  return await runStoredProc(sql);
+  return await runStoredProc({ procedure: sql, name: 'Flat Orders'});
 };
 
 const syncBaseTables = async () => {
@@ -25,7 +25,7 @@ const syncBaseTables = async () => {
       await updateFlatOrders();
       resolve(true);
     } catch (error) {
-      console.error('Error syncing base tables:', error);
+      console.error('❌ Error syncing base tables:', error);
       // Post the error to Slack
       reject(error);
     }
--- app/runner.js
+++ app/runner.js
@@ -3,27 +3,27 @@
 const { connectionPool } = require('./conn/connection');
 const moment = require('moment');
 
-const runStoredProc = (procedure) => {
+const runStoredProc = (meta) => {
   return new Promise((resolve, reject) => {
     // Acquire connection
     connectionPool.getConnection((err, connection) => {
       if (err) {
-        console.error(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: Error acquiring connection >>`, err.message);
+        console.error(`❌ [${moment().format('YYYY-MM-DD HH:mm:ss')}]: Error acquiring connection >>`, err.message);
         reject(err);
         return;
       } else {
         // Run the procedure
-        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: Running procedure:`, procedure);
-        connection.query(procedure, (error, results) => {
+        console.log(`🔄 [${moment().format('YYYY-MM-DD HH:mm:ss')}]: Running procedure:`, meta.procedure);
+        connection.query(meta.procedure, (error, results) => {
           // Release connection
           connection.release();
           if (error) {
-            console.error(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: Error running procedure >>`, error.message);
+            console.error(`❌ [${moment().format('YYYY-MM-DD HH:mm:ss')}]: Error running procedure >>`, error.message);
             reject(error);
             return;
           } else {
             resolve(results);
-            console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: Procedure completed successfully.`);
+            console.log(`✅ [${moment().format('YYYY-MM-DD HH:mm:ss')}]: ${meta.name} completed successfully.`);
           }
         });
       }
